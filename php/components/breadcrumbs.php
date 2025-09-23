78214f7ccc1baa09ea4fadd923f373e2e3251173
Piotr Jozwik <dapeter87@gmail.com>
2025-09-23 21:03:08 +0200

    Add breadcrumb mockup to product

---
+++ php/components/breadcrumbs.php
@@ -0,0 +1,13 @@
+<?php
+function genBreadCrumbs(){
+    ?>
+   <div class="breadcrumbs_wrapper">
+    <ul class="breadcrumbs_wrapper-breadcrumbs">
+        <li class="breadcrumbs_wrapper-breadcrumbs-crumb"><a href="" class="breadcrumbs_wrapper-breadcrumbs-crumb-link">Dom ></a></li>
+        <li class="breadcrumbs_wrapper-breadcrumbs-crumb"><a href="" class="breadcrumbs_wrapper-breadcrumbs-crumb-link">&nbsp; Rakiety ></a></li>
+        <li class="breadcrumbs_wrapper-breadcrumbs-crumb">&nbsp;  Babolat Pure Aero</a></li>
+    </ul>
+   </div>
+
+    <?php
+}
--- product.php
+++ product.php
@@ -1,28 +1,6 @@
 
 <?php
 include './php/layouts/footer.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>10is product</title>
-</head>
-<body>
-  <header>
-    header
-  </header>
-  <main>
-    conetent
-  </main>
-  <?php genFooter() ?>
-</body>
-</html>
-
-
-<?php
-include './php/layouts/footer.php';
 include './php/layouts/header.php';
 include './php/layouts/hero.php';
 include './php/layouts/discounts.php';
@@ -33,28 +11,18 @@ include './php/components/slide.php';
 include './php/components/standard_button.php';
 include './php/components/tile_browser.php';
 include './php/components/tile.php';
-
-
-
-
-
+include './php/components/breadcrumbs.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>10isLife</title>
-  <link rel="stylesheet" href="./css/index.css">
-  <link rel="icon" type="image/x-icon" href="./res/icon/favicon.svg">
-</head>
-<body>
+
   
   <?php genHeader()?>
+  <?php genBreadCrumbs()?>
  <main class="main">
+
+ <h1>Hello product.php</h1>
 
   
 </main>
-  <?php genFooter() ?>
   
+  <?php genFooter() ?>
 
