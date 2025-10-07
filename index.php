28bda89e01f694a751168b64777d35252b7abce3
Piotr Jozwik <83942039+Gitman87@users.noreply.github.com>
2025-10-07 16:38:16 +0200

    Make recommendation section a layout component

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include './php/layouts/footer.php';
 include './php/layouts/header.php';
 include './php/layouts/hero.php';
 include './php/layouts/discounts.php';
+include './php/layouts/recommendation.php';
 
 include './php/components/header_link.php';
 include './php/components/slideshow.php';
@@ -24,33 +25,7 @@ include './php/product.php';
  <main class="main">
 
   <?php genHero() ?>
-  <section class="recommendation">
-    <div class="recommendation-animation">
-        <p class="recommendation-animation-text">
-          <span  style="--i:1;">w</span>
-          <span  style="--i:2;">s</span>
-          <span  style="--i:3;">z</span>
-          <span  style="--i:4;">y</span>
-          <span  style="--i:5;">s</span>
-          <span  style="--i:6;">t</span>
-          <span  style="--i:7;">k</span>
-          <span  style="--i:8;">o</span>
-        </p>
-        <div class="recommendation-animation-container">
-          <img src="./res/icon/favicon.svg" alt="ball" class="recommendation-animation-container-ball">
-        </div>
-    </div>
-    <div class="recommendation-paragraph">
-      <div class="recommendation-paragraph-text">
-      <p class="recommendation-paragraph-text-one">czego</p> 
-      <p class="recommendation-paragraph-text-two">chcesz</p>
-      </div>
-      
-      <img src="./res/img/djoko_signature.webp" alt="djoko signature" class="recommendation-paragraph-signature">
-    </div>
-    <img src="./res/img/djoko.jpg" alt="" class = "recommendation-background">
-
-   </section>
+  <?php genRecommendation() ?>
    <?php genDiscounts() ?>
 
   
