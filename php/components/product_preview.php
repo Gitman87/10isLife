7712076da3ab21f2f969c1a66118a59d77e0b286
Piotr Jozwik <83942039+Gitman87@users.noreply.github.com>
2025-10-07 21:35:45 +0200

    Check and add thumbnail as first preview image in product page

--- php/components/product_preview.php
+++ php/components/product_preview.php
@@ -1,38 +1,40 @@
 <?php
-function genProductPreview($images){
-    ?>
-   <div class="product_preview">
-      <div class="product_preview-preview">
-          <img src="./res/img/products/rackets/babolat/babolat_pure_aero/101479_babolat_01.webp" alt="" class="product_preview-preview-image">
-      </div>
-      <div class="product_preview-slideshow"> 
-        <ul class="product_preview-slideshow-list">  <!-- images will be dynamically added froom db -->
-          <?php 
-          foreach($images as $image){
-            ?>
+function genProductPreview($images)
+{
+  $thumbnailSrc = '';
+  foreach ($images as $image) {
+    if ($image['is_thumbnail'] == 1) {
 
-            <li class="product_preview-slideshow-list-slide"><img src="<?=$image['url']?>" alt="" class="product_preview-slideshow-list-slide-image"></li>
+      $thumbnailSrc = $image['url'];
+    }
+  }
+?>
+  <div class="product_preview">
+    <div class="product_preview-preview">
+      <!-- <img src="res/img/products/rackets/Babolat/100_babolat_01.webp" alt="" class="product_preview-preview-image"> -->
+      <img src="<?= $thumbnailSrc  ?>" alt="" class="product_preview-preview-image">
 
-            <?php 
-          }
-          
-            ?>
-          
-          
-          <!-- <li class="product_preview-slideshow-list-slide"><img src="./res/img/products/rackets/babolat/babolat_pure_aero/101479_babolat_02.webp" alt="" class="product_preview-slideshow-list-slide-image"></li>
-          <li class="product_preview-slideshow-list-slide"><img src="./res/img/products/rackets/babolat/babolat_pure_aero/101479_babolat_03.webp" alt="" class="product_preview-slideshow-list-slide-image"></li>
-          <li class="product_preview-slideshow-list-slide"><img src="./res/img/products/rackets/babolat/babolat_pure_aero/101479_babolat_04.webp" alt="" class="product_preview-slideshow-list-slide-image"></li>
-          <li class="product_preview-slideshow-list-slide"><img src="./res/img/products/rackets/babolat/babolat_pure_aero/101479_babolat_05.webp" alt="" class="product_preview-slideshow-list-slide-image"></li>
-          <li class="product_preview-slideshow-list-slide"><img src="./res/img/products/rackets/babolat/babolat_pure_aero/101479_babolat_05.webp" alt="" class="product_preview-slideshow-list-slide-image"></li>
-          <li class="product_preview-slideshow-list-slide"><img src="./res/img/products/rackets/babolat/babolat_pure_aero/101479_babolat_05.webp" alt="" class="product_preview-slideshow-list-slide-image"></li>
-          <li class="product_preview-slideshow-list-slide"><img src="./res/img/products/rackets/babolat/babolat_pure_aero/101479_babolat_05.webp" alt="" class="product_preview-slideshow-list-slide-image"></li>
-          <li class="product_preview-slideshow-list-slide"><img src="./res/img/products/rackets/babolat/babolat_pure_aero/101479_babolat_05.webp" alt="" class="product_preview-slideshow-list-slide-image"></li>
-          <li class="product_preview-slideshow-list-slide"><img src="./res/img/products/rackets/babolat/babolat_pure_aero/101479_babolat_05.webp" alt="" class="product_preview-slideshow-list-slide-image"></li> -->
-        </ul>
+    </div>
+    <div class="product_preview-slideshow">
+      <ul class="product_preview-slideshow-list"> <!-- images will be dynamically added froom db -->
+        <?php
+        foreach ($images as $image) {
+        ?>
 
-      </div>
+          <li class="product_preview-slideshow-list-slide"><img src="<?= $image['url'] ?>" alt="" class="product_preview-slideshow-list-slide-image"></li>
+
+        <?php
+        }
+
+        ?>
+
+
+
+      </ul>
 
     </div>
 
-    <?php
+  </div>
+
+<?php
 }
