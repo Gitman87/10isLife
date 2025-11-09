2b0f81588dfbc3a53a63a2fdf8342119e60931ae
Piotr Jozwik <83942039+Gitman87@users.noreply.github.com>
2025-11-09 18:28:29 +0100

    Remove getFullName function

--- php/components/review_tiles.php
+++ php/components/review_tiles.php
@@ -50,29 +50,3 @@ function genReviewTiles($prodReviews)
     $time = ''; -->
 <?php
 }
-// function customerFullName($idArray)
-// {
-//     global $user, $host, $password, $db_name;
-//     // Create connection
-//     $conn = new mysqli($host, $user, $password, $db_name);
-//     // Check connection
-//     if ($conn->connect_error) {
-//         die("Connection failed: " . $conn->connect_error);
-//     }
-//     $customersFullNameArray = [];
-//     for ($i = 0; $i < count($idArray); $i++) {
-//         $stmt = $conn->prepare("SELECT first_name, last_name FROM customers WHERE customer_id=?");
-//         if (!$stmt) {
-//             die("statement error" . $conn->error);
-//         }
-//         // echo ($idArray[0]);
-//         $stmt->bind_param("s", $idArray[$i]);
-//         $stmt->execute();
-//         $result = $stmt->get_result();
-//         $assocResult = $result->fetch_assoc();
-//         $fullName = $assocResult['first_name'] . ' ' . $assocResult['last_name'];
-//         $customersFullNameArray[] = $fullName;
-//     }
-
-//     return $customersFullNameArray;
-// }
