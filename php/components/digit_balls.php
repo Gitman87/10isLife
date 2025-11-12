0dc4bdf32e54b29e3d399807c2df596d365bbf2d
Piotr Jozwik <83942039+Gitman87@users.noreply.github.com>
2025-11-12 22:25:21 +0100

    change didgit balls into radio inputs; make input insivisble and stylize label as ball

--- php/components/digit_balls.php
+++ php/components/digit_balls.php
@@ -1,12 +1,16 @@
 <?php
-function genDigitBalls($array)
+function genDigitBalls($array, $name = 'digit_ball')
 {
 ?>
     <ul class="digit_balls">
         <?php
         for ($i = 0; $i < count($array); $i++) {
+            $id = $name . '_' . $array[$i]['value'];
         ?>
-            <li class="digit_balls-ball"><button class="digit_balls-ball-button"><?= $array[$i]['value'] ?></button></li>
+            <li class="digit_balls-item">
+                <input type="radio" name="<?= $name ?>" id="<?= $id ?>" class="digit_balls-item-ball" value="<?= $array[$i]['value'] ?>">
+                <label for="<?= $id ?>" class="digit_balls-item-label"><?= $array[$i]['value'] ?></label>
+            </li>
         <?php
         }
         ?>
