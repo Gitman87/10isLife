e6391a9f7d26fc7317d2fe9637fe6c5e5282cf1b
Piotr Jozwik <83942039+Gitman87@users.noreply.github.com>
2025-12-04 22:01:39 +0100

    Add order number to basket list

--- js/basket_manager.js
+++ js/basket_manager.js
@@ -12,6 +12,9 @@ function basketManager(cartKey) {
   cartListItem.classList.add("basket_content-list");
 
   function updateBasket(cartData) {
+    //for checking if the cartitems has 'options" property -is config or basic
+    const isEmpty = (obj) => Object.keys(obj).length === 0;
+    let i = 1; //for order
     // check if empty, whe user removed all the items
     if (!cartData || cartData.length < 1) {
       cartListContainer.appendChild(emptyBasketInfo);
@@ -19,28 +22,44 @@ function basketManager(cartKey) {
       cartListContainer.innerHTML = "";
       console.log("cartData is: ", cartData);
       cartData.forEach((cartItem) => {
-        cartListContainer.innerHTML += `
+        console.log("cart item optins are: ", isEmpty(cartItem["options"]));
+        if (isEmpty(cartItem["options"])) {
+          cartListContainer.innerHTML += `
           <li class="basket_content-list-row_container" data-row_id=${cartItem["id"]}>
+            <p class="basket_content-list-row_container-order">${i}.</p>
             <img
               src="${cartItem["thumbnail"]}"
               class="basket_content-list-row_container-thumbnail"
             />
             <p class="basket_content-list-row_container-name">${cartItem["fullName"]}</p>
-            <p class="basket_content-list-row_container-grip">Uchwyt:${cartItem["options"]["grip_size"]}</p>
-            <p class="basket_content-list-row_container-length">Długość${cartItem["options"]["length"]}</p>
-            <label>Ilość:</label>
-            <input type ='number' id='basket_unit_quantity' class="basket_content-list-row_container-quantity" name = 'basket_unit_quantity' min=0 max=${cartItem["stockQuantity"]} value=${cartItem["quantity"]}>
+            <div class="basket_content-list-row_container-quantity"
+            <label for="basket_unit_quantity" class="basket_content-list-row_container-quantity-label">Ilość:</label>
+            <input type = 'number' id='basket_unit_quantity' class="basket_content-list-row_container-quantity-input"class="basket_content-list-row_container-quantity" name = 'basket_unit_quantity' min=0 max=${cartItem["stockQuantity"]} value=${cartItem["quantity"]}>
+            </div>
 
             <button class="basket_content-list-row_container-remove_button" onclick="removeCartItem('cart', ${cartItem["id"]})">Usuń</button>
           </li>
         `;
-
-        // const listItem = document.createElement("li");
-        // listItem.classList.add("basket_content-list-row_container");
-        // const thumbnail = document.createElement("img");
-        // thumbnail.classList.add("basket_content-list-row_container-thumbnail");
-        // const productName = document.createElement("p");
-        // productName.classList.add("basket_content-list-row_container-name");
+        } else {
+          cartListContainer.innerHTML += `
+          <li class="basket_content-list-row_container" data-row_id=${cartItem["id"]}>
+            <p class="basket_content-list-row_container-order">${i}.</p>
+            <img
+              src="${cartItem["thumbnail"]}"
+              class="basket_content-list-row_container-thumbnail"
+            />
+            <p class="basket_content-list-row_container-name">${cartItem["fullName"]}</p>
+            <p class="basket_content-list-row_container-grip">Uchwyt: &#32; ${cartItem["options"]["grip_size"]}</p>
+            <p class="basket_content-list-row_container-length">Długość: &#32; ${cartItem["options"]["length"]}</p>
+              <div class="basket_content-list-row_container-quantity"
+            <label for="basket_unit_quantity" class="basket_content-list-row_container-quantity-label">Ilość:</label>
+            <input type = 'number' id='basket_unit_quantity' class="basket_content-list-row_container-quantity-input"class="basket_content-list-row_container-quantity" name = 'basket_unit_quantity' min=0 max=${cartItem["stockQuantity"]} value=${cartItem["quantity"]}>
+            </div>
+            <button class="basket_content-list-row_container-remove_button" onclick="removeCartItem('cart', ${cartItem["id"]})">Usuń</button>
+          </li>
+        `;
+        }
+        i++;
       });
     }
   }
