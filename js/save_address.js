04c06378cfa8097d27f9085938511a3d4c24fb2e
Piotr Jozwik <83942039+Gitman87@users.noreply.github.com>
2025-12-08 20:39:57 +0100

    cleand adding current adress files

--- js/save_address.js
+++ js/save_address.js
@@ -1,6 +1,5 @@
 function saveCurrentAddress(addressKey) {
   let localStorageManager = new LocalStorageManager();
   const currentAddress = window.location.href;
-  console.log("Current address to be written is: ", currentAddress);
   localStorageManager.update(addressKey, currentAddress);
 }
